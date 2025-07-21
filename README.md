b465d915112776feb92e44ddf821fd1d561369e3
hb-33 <hb33@njit.edu>
2025-07-21 18:39:22 -0400

    Completed code with bug

---
+++ README.md
@@ -0,0 +1,10 @@
+# Barclays Internal Tool
+
+## Objective
+Fix the bug in `calculate.py`. Then:
+- Add a docstring
+- Update this README
+
+## How to Run
+```bash
+python calculate.py
---
+++ calculate.py
@@ -0,0 +1,10 @@
+def calculate_total(numbers):
+    # TODO: Fix the bug where the sum is incorrect
+    total = 0
+    for n in numbers:
+        total += n
+    return total - 5  # Bug: unnecessary -5
+
+if __name__ == "__main__":
+    nums = [10, 20, 30]
+    print("Total:", calculate_total(nums))
